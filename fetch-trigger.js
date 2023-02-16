ef6a71aaef2e55d099579d8a859fe0e755346113
srgshpp <108017275+srgshpp@users.noreply.github.com>
2023-02-16 21:33:14 +0300

    Update fetch-trigger.js

--- fetch-trigger.js
+++ fetch-trigger.js
@@ -1,3 +1,5 @@
 const fetch = require('node-fetch');
 
-fetch('https://triangular-heathered-mimosa.glitch.me/');
+fetch('https://triangular-heathered-mimosa.glitch.me/'); // selhoz perepis
+
+fetch('https://playful-jade-wish.glitch.me/'); // metrycs
